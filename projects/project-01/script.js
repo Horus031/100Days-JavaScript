1be59473c2095e612cdaf48f2dd09a972573bcf9
Horus031 <vominhnghia1878@gmail.com>
2025-01-21 14:01:29 +0700

    update final welcome layout

---
+++ projects/project-01/script.js
@@ -0,0 +1,10 @@
+const headerBlock = document.querySelector('#header');
+const mainBlock = document.querySelector('#main');
+
+function switchTab() {
+    headerBlock.classList.add('animate-fadeSequence');
+    mainBlock.classList.add('animate-fadeSequence');
+    setTimeout(function() {
+        window.location.href = "./index.html";
+    }, 1000);
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./projects/**/*.html"],
+  content: [
+    "./projects/**/*.{html,js}",
+    "./projects/**/*.js",
+    "./projects/**/script.js",
+  ],
   theme: {
     extend: {
-      animation: {
-        'fadeout': 'fadeOut 0.3s ease',
-      },
       keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        },
         fadeOut: {
-          '0%': { opacity: '1'},
-          '100%': { opacity: '0'},
+          '0%': { opacity: 1 },
+          '100%': { opacity: 0 },
+        },
+        translateTop: {
+          '0%' : { transform: 'translateY(0)' },
+          '100%': { transform: 'translateY(-100px)'}
         }
-      }
+      },
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+        fadeSequence: 'translateTop 0.5s ease, fadeOut 0.5s ease forwards',
+      },
+      
     },
   },
   plugins: [],
